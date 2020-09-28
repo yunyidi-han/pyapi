e419fff8caa8f6d59daeb7ba7fe66966b60283f2
Yunyidi-Han <yunyidi.han@gmail.com>
2020-09-28 19:20:16 +0000

    learning about lists

---
+++ makejson02.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# JSON is part of the Python Standard Library
+import json
+
+def main():
+    ## create a blob of data to work with
+    hitchhikers = [{"name": "Zaphod Beeblebrox", "species": "Betelgeusian"},
+      {"name": "Arthur Dent", "species": "Human"}]
+        
+    ## display our Python data (a list containing two dictionaries)
+    print(hitchhikers)
+    
+    ## Create the JSON string
+    jsonstring = json.dumps(hitchhikers)
+    
+    ## Display a single string of JSON
+    print(jsonstring)
+
+if __name__ == "__main__":
+    main()
+
